6329800e4912de9692974b8cc72b2db3155caaa5
dionj <dionjbehre@hotmail.com>
2019-01-26 09:01:49 +0100

    option buttons sort of done

--- Assets/Scripts/RadialMenu.cs
+++ Assets/Scripts/RadialMenu.cs
@@ -1,23 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class RadialMenu : MonoBehaviour
 {
-
-    [SerializeField] private Image optionsImage;
+    [SerializeField] private GameObject middleImage;
+    [SerializeField] private Image interactImage;
+    [SerializeField] private Image examineImage;
+    [SerializeField] private Image mouthImage;
     [SerializeField] private Camera cam;
 
     Ray ray;
     RaycastHit hit;
+    Image[] optionsImages;
+    CanvasRenderer[] canvasRenderers;
 
-    CanvasRenderer canvasRenderer;  
-        // Start is called before the first frame update
+    bool showingMenu;
+
+    // Start is called before the first frame update
     void Start()
     {
-        canvasRenderer = optionsImage.gameObject.GetComponent<CanvasRenderer>();
-        canvasRenderer.SetAlpha(0);
+        optionsImages = new Image[] { interactImage, examineImage, mouthImage };
+        canvasRenderers = new CanvasRenderer[] { interactImage.gameObject.GetComponent<CanvasRenderer>(), examineImage.gameObject.GetComponent<CanvasRenderer>(), mouthImage.gameObject.GetComponent<CanvasRenderer>() };
+        foreach (CanvasRenderer canvas in canvasRenderers)
+        {
+            canvas.SetAlpha(0);
+        }
+
+        showingMenu = false;
     }
 
     // Update is called once per frame
@@ -27,36 +39,88 @@ public class RadialMenu : MonoBehaviour
 
         ShowMenuOnMouseClick();
 
-        if  (canvasRenderer.GetAlpha() <= 0.05)
+        for (int i = 0; i < canvasRenderers.Length; i++)
         {
-            canvasRenderer.SetAlpha(0);
-            optionsImage.gameObject.SetActive(false);
+            if (canvasRenderers[i].GetAlpha() <= 0.05f)
+            {
+                canvasRenderers[i].SetAlpha(0);
+                middleImage.gameObject.SetActive(false);
+            }
         }
 
-        print(canvasRenderer.GetAlpha());
     }
     void MoveOptionsMenu()
     {
-        optionsImage.gameObject.transform.position = Input.mousePosition;
+        middleImage.gameObject.transform.position = Input.mousePosition;
     }
 
     void ShowMenuOnMouseClick()
     {
-        if (Physics.Raycast(ray, out hit) )
+        if (Physics.Raycast(ray, out hit))
         {
-            if(hit.collider.tag == "Interactable")
+            if (hit.collider.tag == "Interactable" && !showingMenu)
             {
                 if (Input.GetMouseButtonDown(0))
                 {
-                    optionsImage.gameObject.SetActive(true);
-                    optionsImage.CrossFadeAlpha(1, 0.25f, false);
-                    MoveOptionsMenu();
+                    showingMenu = true;
+
+                    middleImage.gameObject.SetActive(true);
+                    for (int i = 0; i < optionsImages.Length; i++)
+                    {
+                        canvasRenderers[i].SetAlpha(0);
+                        optionsImages[i].CrossFadeAlpha(1, 0.25f, false);
+                        MoveOptionsMenu();
+                    }
+                }              
+            }            
+            /*if (hit.collider.tag == "NotInteractable")
+            {
+                for (int i = 0; i < optionsImages.Length; i++)
+                {
+                    optionsImages[i].CrossFadeAlpha(0, 0.25f, false);
                 }
+            }*/
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            for (int i = 0; i < optionsImages.Length; i++)
+            {
+                optionsImages[i].CrossFadeAlpha(0, 0.25f, false);
+                showingMenu = false;
             }
         }
-        else
+    }
+
+
+    public void InteractButton()
+    {
+        for (int i = 0; i < optionsImages.Length; i++)
         {
-            optionsImage.CrossFadeAlpha(0,0.25f,false);
+            optionsImages[i].CrossFadeAlpha(0, 0.25f, false);
+            showingMenu = false;
         }
+        Debug.Log("InteractButton Pressed");
+
+    }
+
+    public void ExamineButton()
+    {
+        for (int i = 0; i < optionsImages.Length; i++)
+        {
+            optionsImages[i].CrossFadeAlpha(0, 0.25f, false);
+            showingMenu = false;
+        }
+        Debug.Log("Examine Pressed");
+
+    }
+
+    public void MouthButton()
+    {
+        for (int i = 0; i < optionsImages.Length; i++)
+        {
+            optionsImages[i].CrossFadeAlpha(0, 0.25f, false);
+            showingMenu = false;
+        }
+        Debug.Log("MouthButton Pressed");
     }
 }
