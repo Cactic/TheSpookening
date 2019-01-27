c0ddfbc1ab80ea55ae17861a2f096324503187b7
MSI\Claudia <rtclaudiag@gmail.com>
2019-01-27 14:44:25 +0100

    start menu

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void CreditsActive()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void HomeScreen()
+    {
+        SceneManager.LoadScene("StartScreen");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+}
